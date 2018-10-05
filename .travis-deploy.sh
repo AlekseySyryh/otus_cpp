c82763d46bca0a5c3be18a1d73eb54724889d44a
Aleksey Syryh <asyryh@gmail.com>
2018-10-05 16:04:41 +0500

    Cosmetic changes

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-curl -T helloworld-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb -ualekseysyryh:$BINTRAY_API_KEY "https://api.bintray.com/content/alekseysyryh/otus-cpp/homework/$TRAVIS_BUILD_NUMBER/helloworld-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1"
+curl \
+   -T helloworld-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb \
+   -ualekseysyryh:$BINTRAY_API_KEY \
+   https://api.bintray.com/content/alekseysyryh/otus-cpp/homework/$TRAVIS_BUILD_NUMBER/helloworld-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1
 
 body='{
 "request": {
@@ -10,6 +13,6 @@ curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
-   -H "Authorization: token $TRAVISTOKEN" \
+   -H "Authorization: token $TRAVIS_API_TOKEN" \
    -d "$body" \
    https://api.travis-ci.org/repo/AlekseySyryh%2Fotuscpp_docker/requests
