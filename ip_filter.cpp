4c2a4fc77a82c19c63df221c058a46a9316306f1
Aleksey Syryh <asyryh@gmail.com>
2018-10-16 02:02:03 +0500

    Implemented

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,6 +1,10 @@
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <array>
+#include <tuple>
+#include <sstream>
+#include <algorithm>
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -25,90 +29,87 @@ std::vector<std::string> split(const std::string &str, char d) {
     return r;
 }
 
+
+using ipAddressTuple = std::tuple<int, int, int, int>;
+
+struct ipAddress {
+    explicit ipAddress(const std::string &ip) : digits() {
+        auto digitsStrings = split(ip, '.');
+        if (digitsStrings.size() != 4) {
+            throw std::logic_error("Wrong ip");
+        }
+        for (int i = 0; i < 4; ++i) {
+            digits[i] = std::stoi(digitsStrings[i]);
+            if (digits[i] < 0 || digits[i] > 255) {
+                throw std::logic_error("Wrong ip");
+            }
+        }
+        tuple = std::make_tuple(digits[0], digits[1], digits[2], digits[3]);
+    }
+
+    ipAddressTuple tuple;
+
+    std::string getAddress() const {
+        std::ostringstream oss;
+        bool first = true;
+        for (auto d : digits) {
+            if (!first) {
+                oss << ".";
+            } else {
+                first = false;
+            }
+            oss << d;
+
+        }
+        return oss.str();
+    }
+
+    bool isMatch(int a) const {
+        return digits[0] == a;
+    }
+
+    bool isMatch(int a, int b) const {
+        return digits[0] == a && digits[1] == b;
+    }
+
+    bool isMatchAny(int digit) const {
+        return std::any_of(digits.begin(), digits.end(), [digit](const auto d) { return d == digit; });
+    }
+
+private:
+    std::array<int, 4> digits;
+};
+
 int main(int argc, char const *argv[]) {
     try {
-        std::vector<std::vector<std::string>> ip_pool;
+        std::vector<ipAddress> ip_pool;
 
         for (std::string line; std::getline(std::cin, line);) {
             std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            ip_pool.emplace_back(v.at(0));
         }
 
-        // TODO reverse lexicographically sort
+        std::sort(ip_pool.rbegin(), ip_pool.rend(),
+                  [](const auto &lhs, const auto &rhs) { return lhs.tuple < rhs.tuple; });
 
-        for (std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip) {
-            for (std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part) {
-                if (ip_part != ip->cbegin()) {
-                    std::cout << ".";
-
-                }
-                std::cout << *ip_part;
+        for (const auto &ip : ip_pool) {
+            std::cout << ip.getAddress() << std::endl;
+        }
+        for (const auto &ip : ip_pool) {
+            if (ip.isMatch(1)) {
+                std::cout << ip.getAddress() << std::endl;
             }
-            std::cout << std::endl;
         }
-
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
+        for (const auto &ip : ip_pool) {
+            if (ip.isMatch(46, 70)) {
+                std::cout << ip.getAddress() << std::endl;
+            }
+        }
+        for (const auto &ip : ip_pool) {
+            if (ip.isMatchAny(46)) {
+                std::cout << ip.getAddress() << std::endl;
+            }
+        }
     }
     catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
