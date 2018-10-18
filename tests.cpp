6fb8c392d02751388acd060f88703658dca809bd
Aleksey Syryh <asyryh@gmail.com>
2018-10-18 23:12:57 +0300

    Tests fixed

--- tests.cpp
+++ tests.cpp
@@ -1,9 +1,10 @@
-#define BOOST_TEST_MODULE test
-
 #include "split.h"
 #include "ipaddress.h"
 #include "process.h"
 #include <vector>
+
+#define BOOST_TEST_MODULE test_main
+
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_split)
