c1b36d8a64e27b2c96a3b9fb82173a69b034ec5f
Aleksey Syryh <asyryh@gmail.com>
2018-10-27 18:09:03 +0300

    Убрал велосипед

--- processor.h
+++ processor.h
@@ -1,10 +1,10 @@
-#include <utility>
-
 #pragma once
 
+#include <utility>
 #include "ipaddress.h"
 #include <vector>
 #include <functional>
+#include <algorithm>
 
 struct processor {
     explicit processor(
@@ -23,19 +23,45 @@ struct processor {
 
     template<typename ...Args>
     void filter(std::vector<ipAddress> &ip_pool, Args... args) const {
-        for (const auto &ip : ip_pool) {
+        /* Вообще тут было
+           for (const auto &ip : ip_pool) {
             if (ip.isMatch(args...)) {
                 func(ip.getString());
             }
         }
+        Но после вчерашней лекции я понял как был не прав - ведь линейный поиск уже есть в
+        стандартной библиотеке, а я коварно навелосипедил...
+
+         Исправляюсь:*/
+        auto it = ip_pool.begin();
+        while ((it = std::find_if(
+                it,
+                ip_pool.end(),
+                [args...](const auto &ip) {
+                    return ip.isMatch(args...);
+                })) != ip_pool.end()) {
+            func(it->getString());
+            ++it;
+        }
+        //P.S. Извините не удержался... :)
     }
 
 
     void filterAny(std::vector<ipAddress> &ip_pool, int value) const {
-        for (const auto &ip : ip_pool) {
+        /*for (const auto &ip : ip_pool) {
             if (ip.isMatchAny(value)) {
                 func(ip.getString());
             }
+        }*/
+        auto it = ip_pool.begin();
+        while ((it = std::find_if(
+                it,
+                ip_pool.end(),
+                [value](const auto &ip) {
+                    return ip.isMatchAny(value);
+                })) != ip_pool.end()) {
+            func(it->getString());
+            ++it;
         }
     }
 
