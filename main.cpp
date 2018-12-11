753210a7aaf8ea3da315ac5d222c0f80b7c93aef
Aleksey Syryh <asyryh@gmail.com>
2018-12-11 23:10:57 +0300

    Заготовки

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <ctime>
+#include <limits>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    std::time_t start_time = std::time(nullptr);
+    if (argc != 2) {
+        std::cerr << "Следует указать единственный параметр - размер блока" << std::endl;
+        return 1;
+    }
+    int block_size;
+    try {
+        block_size = std::stoi(argv[1]);
+        if (block_size <= 0) {
+            throw std::out_of_range("");
+        }
+    }
+    catch (std::logic_error &) {
+        std::cerr << "Размер блока должен быть целым, положительным числом, не более "
+                  << std::numeric_limits<int>::max() << std::endl;
+        return 1;
+    }
+
+
+    std::cout << start_time << " " << block_size << std::endl;
     return 0;
 }
