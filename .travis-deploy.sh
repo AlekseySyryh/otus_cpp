801a6f90bb0d4b9dce38e4feab018d36a808a15e
Aleksey Syryh <asyryh@gmail.com>
2019-01-03 16:25:43 +0500

    fix

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -2,7 +2,7 @@
 curl \
    -T bulk-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb \
    -ualekseysyryh:$BINTRAY_API_KEY \
-   "https://api.bintray.com/content/alekseysyryh/otus-cpp/homework/$TRAVIS_BUILD_NUMBER/bulk-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64"
+   "https://api.bintray.com/content/alekseysyryh/otus-cpp/homework/$TRAVIS_BUILD_NUMBER/bulk-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1"
 
 body='{
 "request": {
