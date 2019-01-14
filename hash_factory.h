d1567f7865ea34e9b9e7712acfccec2cafbef6ec
Aleksey Syryh <asyryh@gmail.com>
2019-01-15 00:05:08 +0500

    hash factory + правки

---
+++ hash_factory.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#ifdef WITH_OPENSSL
+
+#include <openssl/sha.h>
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+
+#endif
+
+#include <boost/functional/hash.hpp>
+#include <boost/crc.hpp>
+
+#include <boost/container/vector.hpp>
+#include <boost/function.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/foreach.hpp>
+
+template<typename T>
+boost::container::vector<unsigned char> toVector(T value) {
+    boost::container::vector<unsigned char> ret(sizeof(value));
+    for (int i = 0; i < sizeof(value); ++i) {
+        ret[i] = value % 256;
+        value /= 256;
+    }
+    return ret;
+}
+
+boost::container::vector<unsigned char> getCrcHash(boost::container::vector<unsigned char> &buff) {
+    boost::crc_32_type result;
+    result.process_bytes(buff.data(), buff.size());
+    auto res = result.checksum();
+    return toVector(res);
+};
+
+boost::container::vector<unsigned char> getTR1Hash(boost::container::vector<unsigned char> &buff) {
+    boost::hash<std::string> hasher;
+    std::size_t res = hasher(std::string((reinterpret_cast<char *>(buff.data())), buff.size()));
+    return toVector(res);
+}
+
+#ifdef WITH_OPENSSL
+
+boost::container::vector<unsigned char> getMD4Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(MD4_DIGEST_LENGTH);
+    MD4(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+boost::container::vector<unsigned char> getMD5Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(MD5_DIGEST_LENGTH);
+    MD5(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+boost::container::vector<unsigned char> getSHA1Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(SHA_DIGEST_LENGTH);
+    SHA1(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+boost::container::vector<unsigned char> getSHA224Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(SHA224_DIGEST_LENGTH);
+    SHA224(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+boost::container::vector<unsigned char> getSHA256Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(SHA256_DIGEST_LENGTH);
+    SHA256(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+boost::container::vector<unsigned char> getSHA384Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(SHA384_DIGEST_LENGTH);
+    SHA384(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+boost::container::vector<unsigned char> getSHA512Hash(boost::container::vector<unsigned char> &buff) {
+    boost::container::vector<unsigned char> ret(SHA512_DIGEST_LENGTH);
+    SHA512(buff.data(), buff.size(), ret.data());
+    return ret;
+}
+
+#endif
+
+class hashFactory {
+public:
+    hashFactory() {
+        functions["CRC"] = getCrcHash;
+        functions["TR1"] = getTR1Hash;
+#ifdef WITH_OPENSSL
+        functions["MD4"] = getMD4Hash;
+        functions["MD5"] = getMD5Hash;
+        functions["SHA1"] = getSHA1Hash;
+        functions["SHA224"] = getSHA224Hash;
+        functions["SHA256"] = getSHA256Hash;
+        functions["SHA384"] = getSHA384Hash;
+        functions["SHA512"] = getSHA512Hash;
+#endif
+    }
+
+    auto getFunction(std::string &algorythm) {
+        return functions.at(algorythm);
+    }
+
+    auto getAlgorythms() {
+        boost::container::vector<std::string> keys;
+        keys.reserve(functions.size());
+        BOOST_FOREACH(auto rec, functions) {
+                        keys.push_back(rec.first);
+                    }
+        return keys;
+    }
+
+private:
+    boost::unordered_map<
+            std::string,
+            boost::function<boost::container::vector<unsigned char>(
+                    boost::container::vector<unsigned char> &buff)>> functions;
+};
--- main.cpp
+++ main.cpp
@@ -1,98 +1,54 @@
 #include <cstdio>
-
-#ifdef WITH_OPENSSL
-
-#include <openssl/sha.h>
-#include <openssl/md4.h>
-#include <openssl/md5.h>
-
-#endif
-
-#include <boost/functional/hash.hpp>
+#include "hash_factory.h"
 #include <boost/filesystem.hpp>
 #include <iostream>
-#include <boost/container/vector.hpp>
 #include <boost/unordered_set.hpp>
-#include <boost/crc.hpp>
+
 
 int main() {
     using namespace boost::container;
     using namespace boost::filesystem;
     using namespace boost;
-
     vector<path> files;
     unordered_set<path> exclusions;
-    path p1{"/mnt/c/Users/asyry/Desktop"};
-    exclusions.emplace("/mnt/c/Users/asyry/Desktop/Shell");
+    path p1{canonical("..")};
+    exclusions.emplace(canonical("."));
     recursive_directory_iterator it(p1), end;
     while (it != end) {
-        if (it.depth() >= 1 ||
-            exclusions.count(it->path()) > 0) {
-            it.no_push();
-        }
         file_status fs = it->status();
         if (is_regular_file(fs)) {
             files.emplace_back(it->path());
+        } else if (it.depth() > 0 ||
+                   exclusions.count(it->path()) > 0) {
+            it.no_push();
         }
         ++it;
     }
     for (auto &p : files) {
         std::cout << p << std::endl;
     }
-    unsigned char ibuf[] = "compute hash";
-#ifdef WITH_OPENSSL
-    unsigned char obuf[SHA512_DIGEST_LENGTH];
-    int i;
 
-    {
-        printf("\nSHA1\n");
-        SHA1(ibuf, 12, obuf);
-        for (i = 0; i < SHA_DIGEST_LENGTH; i++) {
-            printf("%02x", obuf[i]);
-        }
+    hashFactory factory;
+    boost::container::vector<unsigned char> buff{1, 2, 3, 4, 5, 6};
+    boost::container::vector<unsigned char> buff2{1, 2, 3, 4, 5, 6};
+    boost::container::vector<unsigned char> buff3{1, 2, 3, 4, 5, 6, 7};
+    auto algs = factory.getAlgorythms();
+    for (auto &alg : algs) {
+        std::cout << std::endl << alg << std::endl;
+        auto ret = factory.getFunction(alg)(buff);
+        BOOST_FOREACH(int c, ret) {
+                        std::cout << std::hex << c;
+                    }
+        std::cout << std::endl;
+        ret = factory.getFunction(alg)(buff2);
+        BOOST_FOREACH(int c, ret) {
+                        std::cout << std::hex << c;
+                    }
+        std::cout << std::endl;
+        ret = factory.getFunction(alg)(buff3);
+        BOOST_FOREACH(int c, ret) {
+                        std::cout << std::hex << c;
+                    }
     }
-    {
-        printf("\nSHA224\n");
-        SHA224(ibuf, 12, obuf);
-        for (i = 0; i < SHA224_DIGEST_LENGTH; i++) {
-            printf("%02x", obuf[i]);
-        }
-    }
-    {
-        printf("\nSHA256\n");
-        SHA256(ibuf, 12, obuf);
-        for (i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-            printf("%02x", obuf[i]);
-        }
-    }
-    printf("\nSHA384\n");
-    SHA384(ibuf, 12, obuf);
-    for (i = 0; i < SHA384_DIGEST_LENGTH; i++) {
-        printf("%02x", obuf[i]);
-    }
-    printf("\nSHA512\n");
-    SHA512(ibuf, 12, obuf);
-    for (i = 0; i < SHA512_DIGEST_LENGTH; i++) {
-        printf("%02x", obuf[i]);
-    }
-    printf("\nMD4\n");
-    MD4(ibuf, 12, obuf);
-    for (i = 0; i < MD4_DIGEST_LENGTH; i++) {
-        printf("%02x", obuf[i]);
-    }
-    printf("\nMD5\n");
-    MD5(ibuf, 12, obuf);
-    for (i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        printf("%02x", obuf[i]);
-    }
-#endif
-    printf("\nTR1\n");
-    hash<unsigned char *> string_hash;
-    std::size_t h = string_hash(ibuf);
-    std::cout << h;
-    boost::crc_32_type result;
-    result.process_bytes(ibuf, 12);
-    printf("\nCRC\n");
-    std::cout << result.checksum();
     return EXIT_SUCCESS;
 }
