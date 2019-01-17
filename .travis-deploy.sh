ea637e37f939b50194ce168599586d2d41fd1e52
Aleksey Syryh <asyryh@gmail.com>
2019-01-17 17:01:10 +0500

    Измененно название проекта

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 curl \
-   -T bulk-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb \
+   -T bulkmt-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb \
    -ualekseysyryh:$BINTRAY_API_KEY \
-   "https://api.bintray.com/content/alekseysyryh/otus-cpp2/homework/$TRAVIS_BUILD_NUMBER/bulk-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
+   "https://api.bintray.com/content/alekseysyryh/otus-cpp2/homework/$TRAVIS_BUILD_NUMBER/bulkmt-0.0.$TRAVIS_BUILD_NUMBER-Linux.deb;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
 body='{
 "request": {
 "branch":"master"
